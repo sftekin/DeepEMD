6f18817c059eb072c5d6d6b2d64a444ffd98fd2c
sftekin <sftekin25@gmail.com>
2022-09-27 22:51:18 +0000

    matching network finished

--- Models/models/baseline_models.py
+++ Models/models/baseline_models.py
@@ -12,10 +12,14 @@ class BaseLineModel(nn.Module):
         self.mode = mode
         self.args = args
 
+        self.k = args.way
+        self.n = args.shot
+        self.q = args.query
+
         self.encoder = ResNet(args)
         self.epsilon = 1e-8
 
-    def forward(self, input, **kwargs):
+    def forward(self, input):
         pass
         
 
@@ -64,13 +68,12 @@ class Prototype(BaseLineModel):
     def __init__(self, args, mode="meta") -> None:
         super().__init__(args, mode)
     
-    def forward(self, input, **kwargs):
+    def forward(self, input):
         if self.mode == "encode":
             return nn.Flatten()(self.encoder(input))
         elif self.mode == "meta":
             support, queries = input
-            k, n = kwargs["k"], kwargs["n"]
-            prototypes = self.compute_prototypes(support, k, n)
+            prototypes = self.compute_prototypes(support)
 
             distances = self.pairwise_distances(queries, prototypes, distance="l2")
             logits = distances.softmax(dim=1)
@@ -78,23 +81,11 @@ class Prototype(BaseLineModel):
         else:
             raise ValueError('Unknown mode')
 
-
-    @staticmethod
-    def compute_prototypes(support: torch.Tensor, k: int, n: int) -> torch.Tensor:
-        """Compute class prototypes from support samples.
-
-        # Arguments
-            support: torch.Tensor. Tensor of shape (n * k, d) where d is the embedding
-                dimension.
-            k: int. "k-way" i.e. number of classes in the classification task
-            n: int. "n-shot" of the classification task
-
-        # Returns
-            class_prototypes: Prototypes aka mean embeddings for each class
-        """
+    def compute_prototypes(self, support):
+        """Compute class prototypes from support samples."""
         # Reshape so the first dimension indexes by class then take the mean
         # along that dimension to generate the "prototypes" for each class
-        class_prototypes = support.reshape(k, n, -1).mean(dim=1)
+        class_prototypes = support.reshape(self.k, self.n, -1).mean(dim=1)
         return class_prototypes
 
 
@@ -106,5 +97,60 @@ class Matching(BaseLineModel):
         if self.mode == "encode":
             return nn.Flatten()(self.encoder(input))
         elif self.mode == "meta":
-            pass
+            support, queries = input
+
+            distances = self.pairwise_distances(queries, queries, distance="cosine")
+            attention = (-distances).softmax(dim=1)
+
+            y_pred = self.matching_net_predictions(attention)
+
+            # Calculated loss with negative log likelihood
+            # Clip predictions for numerical stability
+            clipped_y_pred = y_pred.clamp(self.epsilon, 1 - self.epsilon)
+            
+            return clipped_y_pred
+    
+    def matching_net_predictions(self, attention):
+        """
+        Calculates Matching Network predictions based on equation (1) of the paper.
+        The predictions are the weighted sum of the labels of the support set where the
+        weights are the "attentions" (i.e. softmax over query-support distances) pointing
+        from the query set samples to the support set samples.
+        """
+        # Reshape so the first dimension indexes by class then take the mean
+        # along that dimension to generate the "prototypes" for each class
+        if attention.shape != (self.q * self.k, self.k * self.n):
+            raise(ValueError(f'Expecting attention Tensor to have shape (q * k, k * n) \
+             = ({self.q * self.k, self.k * self.n})'))
+
+        # Create one hot label vector for the support set
+        y_onehot = torch.zeros(self.k * self.n, self.k)
+
+        # Unsqueeze to force y to be of shape (K*n, 1) as this
+        # is needed for .scatter()
+        y = self.create_nshot_task_label(self.k, self.n).unsqueeze(-1)
+        y_onehot = y_onehot.scatter(1, y, 1)
+
+        y_pred = torch.mm(attention, y_onehot.cuda().double())
+
+        return y_pred
+
+    @staticmethod
+    def create_nshot_task_label(k: int, q: int) -> torch.Tensor:
+        """Creates an n-shot task label.
+
+        Label has the structure:
+            [0]*q + [1]*q + ... + [k-1]*q
+
+        # TODO: Test this
+
+        # Arguments
+            k: Number of classes in the n-shot classification task
+            q: Number of query samples for each class in the n-shot classification task
+
+        # Returns
+            y: Label vector for n-shot task of shape [q * k, ]
+        """
+        y = torch.arange(0, k, 1 / q).long()
+        return y
 
