42a6d4558f409f640cf90dc6b6ebb7afac6e4bd4
sftekin <sftekin25@gmail.com>
2022-09-27 19:18:14 +0000

    prototype model

--- Models/models/baseline_models.py
+++ Models/models/baseline_models.py
@@ -12,21 +12,11 @@ class BaseLineModel(nn.Module):
         self.mode = mode
         self.args = args
 
-        self.matching_fn = args.matching_fn
         self.encoder = ResNet(args)
         self.epsilon = 1e-8
 
-    def forward(self, input):
-        support, query = input
-
-        # prototype
-        if self.mode == "encode":
-            pass
-        elif self.mode == "meta":
-            pass
-            # matching network
-
-            # fc
+    def forward(self, input, **kwargs):
+        pass
         
 
     def pairwise_distances(self, x: torch.Tensor, y: torch.Tensor, matching_fn: str) -> torch.Tensor:
@@ -68,6 +58,41 @@ class BaseLineModel(nn.Module):
             raise(ValueError('Unsupported similarity function'))
 
         return distances
+
+
+class Prototype(BaseLineModel):
+    def __init__(self, args, mode="meta") -> None:
+        super().__init__(args, mode)
+    
+    def forward(self, input, **kwargs):
+        if self.mode == "encode":
+            return nn.Flatten()(self.encoder(input))
+        else:
+            support, queries = input
+            k, n = kwargs["k"], kwargs["n"]
+            prototypes = self.compute_prototypes(support, k, n)
+
+            distances = self.pairwise_distances(queries, prototypes, distance="l2")
+            
+
+
+    @staticmethod
+    def compute_prototypes(support: torch.Tensor, k: int, n: int) -> torch.Tensor:
+        """Compute class prototypes from support samples.
+
+        # Arguments
+            support: torch.Tensor. Tensor of shape (n * k, d) where d is the embedding
+                dimension.
+            k: int. "k-way" i.e. number of classes in the classification task
+            n: int. "n-shot" of the classification task
+
+        # Returns
+            class_prototypes: Prototypes aka mean embeddings for each class
+        """
+        # Reshape so the first dimension indexes by class then take the mean
+        # along that dimension to generate the "prototypes" for each class
+        class_prototypes = support.reshape(k, n, -1).mean(dim=1)
+        return class_prototypes
 
 
 
