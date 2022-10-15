28fa732a238158076331af70f54aa4974baf35e7
sftekin <sftekin25@gmail.com>
2022-10-15 01:33:07 +0000

    plot the bests of compare

--- baseline_experiments/collecting_negatives/negative_ds.py
+++ baseline_experiments/collecting_negatives/negative_ds.py
@@ -8,6 +8,8 @@ import numpy as np
 import pandas as pd
 
 from Models.utils import ensure_path
+from plotting import plot_support_query
+
 
 class Negatives(Dataset):
 
@@ -55,33 +57,8 @@ class Negatives(Dataset):
         return images
 
 
-def plot_support_query(batch_path, batch_label, query_idx=5):
-    figures_dir = osp.join("/content", "DeepEMD", "outputs", "figures")
-    ensure_path(figures_dir)
-
-    fig, ax = plt.subplots(1, 5, figsize=(8, 10))
-    for i in range(5):
-        im = Image.open(batch_path[i]).convert("RGB")
-        ax[i].imshow(im)
-        ax[i].set_xticks([])
-        ax[i].set_yticks([])
-        ax[i].set_title(f"{batch_label[i]}")
-    fig_path = osp.join(figures_dir, "support.png")
-    plt.savefig(fig_path, dpi=200, bbox_inches="tight")
-
-    #plot query
-    fig, ax = plt.subplots()
-    im = Image.open(batch_path[query_idx]).convert("RGB")
-    ax.imshow(im)
-    ax.set_title(batch_label[query_idx])
-    fig_path = osp.join(figures_dir, "query.png")
-    plt.savefig(fig_path, dpi=200, bbox_inches="tight")
-
-
 
 if __name__ == '__main__':
-    from label_names import label_names
-    import matplotlib.pyplot as plt
 
     negatives_set = Negatives(rule_name="threshold", model_name="DeepEMD")
     for path, image, label in negatives_set.next():
